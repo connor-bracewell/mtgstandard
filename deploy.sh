505edb2b9f4dac7441e6686f57a90b5c82cde859
Connor Bracewell <connor.bracewell@gmail.com>
2019-07-21 20:28:35 -0700

    Update deploy and use mustache for brawl

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 rm -rf web
-mkdir web
-cp src/index.html web
+mkdir web web/standard web/brawl
+mustache src/standard.json src/index.mustache > web/standard/index.html
+mustache src/brawl.json    src/index.mustache > web/brawl/index.html
