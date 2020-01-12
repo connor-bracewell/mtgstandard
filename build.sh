ea6cc63e34dcd946bbb80c86e9d32de0ddfb7051
Connor Bracewell <connor.bracewell@gmail.com>
2020-01-12 10:42:12 -0800

    Update before Theros: Beyond Death prerelease

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 rm -rf web
-mkdir web web/standard web/brawl
-mustache src/standard.json src/index.mustache > web/standard/index.html
-mustache src/brawl.json    src/index.mustache > web/brawl/index.html
+mkdir web web/standard web/brawl tmp
+jq -s '.[0] * .[1]' src/sets.json src/standard.json > tmp/standard_final.json
+jq -s '.[0] * .[1]' src/sets.json src/brawl.json > tmp/brawl_final.json
+mustache tmp/standard_final.json src/index.mustache > web/standard/index.html
+mustache tmp/brawl_final.json    src/index.mustache > web/brawl/index.html
